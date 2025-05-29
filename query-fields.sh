134a4b9af5731f4739c5cb238f884a8a07f86979
Federico Abella <65520926+FedeAbella@users.noreply.github.com>
2025-05-29 16:28:03 -0300

    prettify message

--- query-fields.sh
+++ query-fields.sh
@@ -5,6 +5,6 @@ read -r -p "Enter sObjects to query (space separated list): " -a objects
 read -r -p "Enter a jq select query to filter fields by: " query
 
 for object in "${objects[@]}"; do
-    echo "Querying ${object}..."
+    env printf "\n\e[1;34m-----\n\u279C Querying ${object}...\n-----\e[0m\n"
     sf sobject describe --sobject "${object}" --target-org "${target_org}" | jq "[(.fields | .[] | select(${query})) | .name ]" >"${object}".json
 done
