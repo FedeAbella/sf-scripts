748f068119520b5ce4a544964d94eb1b5a38123a
Federico Abella <65520926+FedeAbella@users.noreply.github.com>
2025-05-29 15:13:43 -0300

    run shellcheck

--- query-fields.sh
+++ query-fields.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-read -p "Enter target org: " target_org
-read -p "Enter sObjects to query (space separated list): " -a objects
-read -p "Enter a jq select query to filter fields by: " query
+read -r -p "Enter target org: " target_org
+read -r -p "Enter sObjects to query (space separated list): " -a objects
+read -r -p "Enter a jq select query to filter fields by: " query
 
-for object in ${objects[@]}; do
+for object in "${objects[@]}"; do
     echo "Querying ${object}..."
-    sf sobject describe --sobject ${object} --target-org ${target_org} | jq "[(.fields | .[] | select(${query})) | .name ]" >${object}.json
+    sf sobject describe --sobject "${object}" --target-org "${target_org}" | jq "[(.fields | .[] | select(${query})) | .name ]" >"${object}".json
 done
